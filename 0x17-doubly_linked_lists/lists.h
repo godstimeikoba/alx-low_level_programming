bb1564c3b0f49fe008702586a620f85e8ba62c9e
godstimeikoba <godstimeikoba@gmail.com>
2022-12-10 18:40:23 -0800

    A function that adds node

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,7 +1,8 @@
 #ifndef _LISTS_H_
 #define _LISTS_H_
 
-#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct dlistint_s - doubly linked list
@@ -29,5 +30,4 @@ int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
-#endif /* _LISTS_H_ */
-
+#endif
